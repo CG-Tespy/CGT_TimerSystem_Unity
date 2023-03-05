4a47f3d9db9aef906fcf01c53407c8d1c5474c76
CG-Tespy <gbcabreracgt@gmail.com>
2023-03-05 08:15:43 -0500

    Big reimport for compatibility with TMPro
    For some reason, despite having TMPro installed through the PackageManager, I was unable to access it in code. So I had to create a new project where it DID work, then copy everything over to this so things would work.
    
    Also did some reorganizing of files, like with the demo scene.

---
+++ Assets/CGT_TimerSystem/TMProSupport/Scripts/TMProUITimerDisplay.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace CGT.Unity.TimerSys
+{
+    public class TMProUITimerDisplay : TimerDisplay
+    {
+        [SerializeField]
+        protected TextMeshProUGUI textField;
+
+        public override void DisplayTime()
+        {
+            UpdateTextField();
+        }
+
+        protected virtual void UpdateTextField()
+        {
+            string timeToDisplayString = TimeToDisplay.ToString(FormatToDisplayIn);
+            textField.text = timeToDisplayString;
+        }
+
+        public override void StopDisplayingTime()
+        {
+            IsDisplayingTime = false;
+        }
+    }
+}
